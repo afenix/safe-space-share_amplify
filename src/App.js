63f5b342515592fb8d0d3afded1b5e83eded66e1
Alister A. Fenix <tech@vpdatacommons.org>
2024-06-30 18:15:57 -0700

    Add base React project scaffolding, with package.json requirements set for building with  github pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MapComponent from './components/MapComponent';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Safe Space</h1>
+      </header>
+      <MapComponent />
+    </div>
+  );
+}
+
+
+export default App;
