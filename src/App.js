fe658457bf2cc125ca56e64d9a15b4538ccde3fe
Alister A. Fenix <alister.fenix@way-find.com>
2024-06-30 22:08:06 -0700

    Prevent ArcGIS JS API from auto focusing map view on page load
    commit fixes bug where website was auto-focusing to map.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+// Code to render the main application
+import React, { useEffect } from 'react';
 import MapComponent from './components/MapComponent';
 import Survey123Form from './components/Survey123Form';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const surveyUrl = "https://arcg.is/1PTu1L";
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +22,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
