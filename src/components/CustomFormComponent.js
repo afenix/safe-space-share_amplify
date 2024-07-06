ef2e3386a73c963fff8e750bcc2f9664786c1b5f
Alister A. Fenix <alister.fenix@way-find.com>
2024-07-05 22:27:41 -0700

    Remove unused refs

--- src/components/CustomFormComponent.js
+++ src/components/CustomFormComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import '../App.css';
 
 const CustomFormComponent =  ({ formData, setFormData, handleChange, handleSubmit }) => {
