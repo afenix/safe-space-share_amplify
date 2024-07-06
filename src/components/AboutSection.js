6ddb23f4696d7f105f03284edf8c512c2e7b960d
Alister A. Fenix <alister.fenix@way-find.com>
2024-07-06 15:10:28 -0700

    Improve about section language
    so that it better matches the tone of the application - as well as is concise enough for mobile viewing

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,17 +6,23 @@ const AboutSection = () => {
     <div className='about-section'>
       <h1 id='about-header'>What is SafeSpaceShare?</h1>
       <p>
-        We all experience public spaces differently. These experiences, shaped
-        by who we are and how we are perceived, profoundly impact our sense of
-        safety, well-being, and belonging, but are rarely seen, yet alone seen
-        collectively.
+        We all navigate public spaces in our unique ways, influenced by who we
+        are and how others see us. These experiences shape our sense of safety,
+        well-being, and belonging, yet they often go unseen, especially as a
+        collective.
       </p>
       <p>
-        SafeSpaceShare (S3) is designed to allow you to anonymously share your
-        experiences, explore how your identity may have shaped them, and
-        discover how others experience the same places.
+        SafeSpaceShare (S3) is here to change that. Anonymously share your
+        stories, explore how your identity influences your experiences, and
+        connect with the diverse perspectives of others. With S3's interactive
+        map, you can uncover patterns and similarities across different
+        communities, helping us all gain deeper insights into our shared and
+        diverse worlds.
       </p>
-      <h2 id='about-subtitle'>Ready to dive in? <br/>Let's get started!</h2>
+      <h2 id='about-subtitle'>
+        Ready to dive in? <br />
+        Let's get started!
+      </h2>
       <img
         id='everyone-matters-icon'
         src={`${process.env.PUBLIC_URL}/everyoneMatters.png`}
