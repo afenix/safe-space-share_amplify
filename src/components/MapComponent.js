8dd11ff38838b54e766083f13e52c4758c39f616
Alister A. Fenix <alister.fenix@way-find.com>
2024-07-05 22:17:24 -0700

    Add additional features and custom styles to map component
    This commit includes updates to improve behavior and styling of map. Such as the addition of features such as search and locate, as well as custom renderer for user point.

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -8,7 +8,9 @@ console.log('MapComponent rendered'); // Add this line
   const [view, setView] = useState(null);
   const [selectedPoint, setSelectedPoint] = useState(null);
 
-  // Utility functions
+  const featureServiceUrl = 'https://services1.arcgis.com/IVzPgL57Mwzk8mu1/arcgis/rest/services/SafeSpaceShare/FeatureServer';
+
+  // Utility functions (most likely to deprecate in the future)
   const fetchSurveyData = async featureServiceUrl => {
     const queryUrl = `${featureServiceUrl}/0/query?where=1%3D1&outFields=*&f=json`
     const response = await fetch(queryUrl)
@@ -33,11 +35,10 @@ console.log('MapComponent rendered'); // Add this line
       },
       [0, 0]
     )
-
     return [sum[0] / total, sum[1] / total]
   }
 
-  // Function to trigger form data update - moved outside the click handler
+  // Function to trigger form data update
   const updateFormDataWithPoint = () => {
     if (selectedPoint) {
       setFormData(prevData => ({
@@ -50,65 +51,161 @@ console.log('MapComponent rendered'); // Add this line
   useEffect(() => {
     const initializeMap = async () => {
       try {
-        const [Map, MapView, Graphic, FeatureLayer] = await loadModules(
+        const [
+          Map,
+          MapView,
+          Graphic,
+          FeatureLayer,
+          GraphicsLayer,
+          PopupTemplate,
+          Editor,
+          Locate,
+          Search,
+          Expand
+        ] = await loadModules(
           [
             'esri/Map',
             'esri/views/MapView',
             'esri/Graphic',
-            'esri/layers/FeatureLayer'
+            'esri/layers/FeatureLayer',
+            'esri/layers/GraphicsLayer',
+            'esri/PopupTemplate',
+            'esri/widgets/Editor',
+            "esri/widgets/Locate",
+            'esri/widgets/Search',
+            'esri/widgets/Expand',
           ],
           { css: true }
-        )
+        );
 
         if (mapRef.current) {
           const map = new Map({
             basemap: 'streets-navigation-vector'
           });
 
-        const view = new MapView({
-          container: mapRef.current,
-          map: map,
-          center: [-122.6765, 45.5231], // Portland center
-          zoom: 13
-        })
+          const mapView = new MapView({
+            container: mapRef.current,
+            map: map,
+            center: [-122.6765, 45.5231],
+            zoom: 13
+          });
 
-        const featureLayer = new FeatureLayer({
-          url: featureServiceUrl
-        })
+          setView(mapView);
 
-        map.add(featureLayer)
+          const userLayer = new GraphicsLayer();
+          map.add(userLayer);
 
-        console.log('Map and View Initialized')
+          // Add a locate button to the view
+          const locateBtn = new Locate({
+            view: mapView
+          });
 
-        view.on('click', event => {
-          const point = {
-            type: 'point',
-            longitude: event.mapPoint.longitude,
-            latitude: event.mapPoint.latitude,
-            spatialReference: view.spatialReference // Use view's spatialReference
+          // Add the locate widget to the top left corner of the view (under the zoom buttons)
+          mapView.ui.add(locateBtn, {
+              position: "top-right"
+          });
+
+
+          // Create the search widget
+          const searchWidget = new Search({
+            view: mapView
+          })
+
+          // Initialize and add the Expand widget to toggle the visibility of the Search widget
+          const searchExpand = new Expand({
+            expandIcon: 'search', // // Use a search icon for the expand/collapse button. See https://developers.arcgis.com/calcite-design-system/icons/
+            expandTooltip: 'Expand Search', // Custom tooltip text for the expand button
+            view: mapView,
+            content: searchWidget
+          })
+
+          mapView.ui.add(searchExpand, 'top-right')
+
+          console.log('Map and View initialized successfully')
+
+          // Notify the parent component when the map is ready
+          if (onMapReady) {
+            onMapReady(mapView); // Pass the MapView instance to the callback
           }
 
-          const symbol = {
-            // Moved symbol definition outside the click handler
-            type: 'simple-marker',
-            color: 'red',
-            size: '8px',
-            outline: {
-              color: [255, 255, 255],
-              width: 1
+          // Create a custom style renderer for user location
+          const locationRenderer = {
+            type: 'simple',
+            symbol: {
+              type: 'picture-marker',
+              url: `${process.env.PUBLIC_URL}/person_pin_circle_24dp_E16833.png`,
+              width: '25px',
+              height: '25px'
             }
           }
 
-          const graphic = new Graphic({
-            geometry: point,
-            symbol: symbol
+          // Create a PopupTemplate
+          const popupTemplate = new PopupTemplate({
+            title: 'Location Details',
+            content: 'Latitude: {latitude}<br>Longitude: {longitude}'
           })
 
-          view.graphics.removeAll()
-          view.graphics.add(graphic)
+          const s3UserLocation = new FeatureLayer({
+            url: featureServiceUrl,
+            renderer: locationRenderer,
+            // add fields the popup here
+            outFields: ["experience_date", "happinessSadness", "calmAnxious", "awakeTired", "safety", "belonging"],
+          })
 
-          setSelectedPoint(point) // Update selectedPoint state
-        })
+          map.add(s3UserLocation)
+          console.log('Feature layer added to the map')
+
+          // Click event handler
+          mapView.on('click', event => {
+            console.log('Map clicked at:', event.mapPoint) // Log click event
+            const point = {
+              type: 'point',
+              longitude: event.mapPoint.longitude,
+              latitude: event.mapPoint.latitude,
+              spatialReference: mapView.spatialReference // Use view's spatialReference
+            }
+
+            const symbol = {
+              type: 'picture-marker',
+              url: `${process.env.PUBLIC_URL}/diversity_2_24dp_E16833.png`,
+              width: '45px',
+              height: '45px'
+            }
+
+            const graphic = new Graphic({
+              geometry: point,
+              symbol: symbol
+            })
+
+            // Remove only the previously selected graphic, not all graphics
+            const existingGraphic = mapView.graphics.find(
+              g =>
+                g.symbol.type === 'picture-marker' &&
+                g.symbol.url.includes('diversity_2_24dp_E16833.png')
+            );
+
+            if (existingGraphic) {
+              mapView.graphics.remove(existingGraphic)
+            };
+
+  // TODO: THIS IS NOT WORKING NEEDS TO BE FIXED
+            // Attach the popupTemplate to the graphic
+            graphic.popupTemplate = popupTemplate
+            mapView.popup.open({
+              features: [graphic],
+              location: event.mapPoint
+            });
+
+            mapView.graphics.add(graphic)
+            setSelectedPoint(point) // Update selectedPoint state
+
+            // Update form data with selected point
+            setFormData(prevData => ({
+              ...prevData,
+              locationName: `${point.longitude}, ${point.latitude}`
+            }));
+          });
+        };
       } catch (err) {
         console.error(err)
       };
@@ -134,13 +231,17 @@ console.log('MapComponent rendered'); // Add this line
   }
 
   return (
-    <div>
-      <div style={{ height: '50vh' }} ref={mapRef}></div>
-      <button onClick={() => updateFormDataWithPoint()}>
-        Add Location to Form
-      </button>
-    </div>
+    <div
+      ref={mapRef}
+      className="map-container"
+      style={{
+        borderRadius: '10px',
+        height: '50vh',
+        width: '100vw',
+        margin: '20px'
+      }}
+    ></div>
   )
-}
+});
 
 export default MapComponent
